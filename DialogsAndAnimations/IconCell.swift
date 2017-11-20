488b5f5319807979c02c133db87f885dd4af9141
hackeru <hackeru@hackerus-Mac.local>
2017-11-20 19:10:56 +0300

    hiday

---
+++ DialogsAndAnimations/IconCell.swift
@@ -0,0 +1,13 @@
+//
+//  IconCell.swift
+//  DialogsAndAnimations
+//
+//  Created by hackeru on 2 Kislev 5778.
+//  Copyright © 5778 hackeru. All rights reserved.
+//
+
+import UIKit
+
+class IconCell: UICollectionViewCell {
+    
+}
