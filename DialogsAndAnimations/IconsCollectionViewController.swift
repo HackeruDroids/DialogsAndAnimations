99f5f664b96b530b41f959adceab4eaab559387a
hackeru <hackeru@hackerus-Mac.local>
2017-11-20 22:31:33 +0300

    animations

--- DialogsAndAnimations/IconsCollectionViewController.swift
+++ DialogsAndAnimations/IconsCollectionViewController.swift
@@ -11,38 +11,38 @@ import UIKit
 
 
 class IconsCollectionViewController: UICollectionViewController {
-//iconCell
+    //iconCell
     private var data = [#imageLiteral(resourceName: "icons8-mario"), #imageLiteral(resourceName: "icons8-novel"), #imageLiteral(resourceName: "icons8-alien"), #imageLiteral(resourceName: "icons8-zubat"), #imageLiteral(resourceName: "icons8-desura"), #imageLiteral(resourceName: "icons8-pokemon"), #imageLiteral(resourceName: "icons8-rattata"), #imageLiteral(resourceName: "icons8-candy_cane"), #imageLiteral(resourceName: "icons8-pokeball_2"), #imageLiteral(resourceName: "icons8-filled_like"), #imageLiteral(resourceName: "icons8-super_mario"), #imageLiteral(resourceName: "icons8-christmas_stocking")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
     // MARK: UICollectionViewDataSource
-
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
         return data.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "iconCell", for: indexPath) as! IconCell
-    
+        
         // Configure the cell
         //get the image using the indexpath
         let image = data[indexPath.item]
@@ -51,43 +51,50 @@ class IconsCollectionViewController: UICollectionViewController {
         
         return cell
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let icon = data[indexPath.item]
         //if we have a listener notify the listener
         listener?(icon)
+        
+        //delegate?.didSelectIcon(image: icon)
     }
     
     public var listener : ((_ image:UIImage)->Void)?
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
-    }
-    */
-
+    //public var delegate: IconCollectionViewDelegate?
+    // MARK: UICollectionViewDelegate
 }
+//public protocol IconCollectionViewDelegate{
+//    func didSelectIcon(image: UIImage)
+//}
+/*
+ // Uncomment this method to specify if the specified item should be highlighted during tracking
+ override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+ return true
+ }
+ */
+
+/*
+ // Uncomment this method to specify if the specified item should be selected
+ override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+ return true
+ }
+ */
+
+/*
+ // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+ override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+ return false
+ }
+ 
+ override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+ return false
+ }
+ 
+ override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+ 
+ }
+ */
+
+//}
--- DialogsAndAnimations/ViewController.swift
+++ DialogsAndAnimations/ViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{//}, IconCollectionViewDelegate {
+//    func didSelectIcon(image: UIImage) {
+//
+//    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,12 +22,48 @@ class ViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let dest = segue.destination as? IconsCollectionViewController else {return}
-        
-        dest.listener =  onImageTapped(_:) // {_ in }
+        //dest.delegate = self
+        dest.listener = onImageTapped(_:) // {_ in print("woot?" + $0.description)}
     }
     
     func onImageTapped(_ image:UIImage){
-        print(image)
+        //init an imageView
+        
+        
+        
+        let imageView = UIImageView(image: image)
+       
+        //let iv = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        //position it:
+        imageView.center = self.view.center
+        
+       
+        //starting point for the animation
+        imageView.transform =  CGAffineTransform(translationX: 0, y: self.view.frame.height)
+
+        UIView.animate(withDuration: 0.7, animations: {
+            imageView.transform = CGAffineTransform.identity
+        }) { _ in
+           // imageView.removeFromSuperview()
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0.7, options: .curveEaseInOut, animations: {
+            imageView.alpha = 0
+        }) { (_) in
+            imageView.removeFromSuperview()
+        }
+        
+//        UIView.animate(withDuration: 0.6) {
+//            //transform: scale, translate, rotate
+//            //imageView.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height )
+//
+//
+//            //end point
+//            imageView.transform = CGAffineTransform.identity //reset all former transformations.
+//        }
+        
+        //add it as a child
+        self.view.addSubview(imageView)
     }
 }
 
